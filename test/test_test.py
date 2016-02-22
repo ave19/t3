dc52f416d7913780c915818059885e2e8606277f
ave19 <dave.jaccard@gmail.com>
2016-02-21 22:24:29 -0700

    fixing whitespace per PEP8

--- test/test_test.py
+++ test/test_test.py
@@ -1,18 +1,19 @@
 import unittest
 
+
 class test_test(unittest.TestCase):
-    
+
     context = None
-    
+
     def setUp(self):
         self.context = True
-        
+
     def test_true(self):
         self.failUnless(True)
-        
+
     def test_context(self):
         self.failUnless(self.context)
-        
-        
+
+
 if __name__ == '__main__':
     unittest.main()
