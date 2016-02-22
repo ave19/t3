0a30745bf73ca3cd796574c9ff6f63bdc662bc6e
ave19 <dave.jaccard@gmail.com>
2016-02-21 22:25:02 -0700

    PEP8 and adding 't3' to the import

--- test/test_player.py
+++ test/test_player.py
@@ -1,17 +1,17 @@
 import unittest
-from player import Player
+from t3.player import Player
 
 class test_player(unittest.TestCase):
-    
+
     p = None
-    
+
     def setUp(self):
         self.p = Player()
-        
+
     def test_class(self):
         self.assertIsInstance(self.p, Player, "p is not Player")
-    
-        
-        
+
+
+
 if __name__ == '__main__':
     unittest.main()
