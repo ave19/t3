f6446387814a1b41d26f0d97a2887ad0f36ca493
ave19 <dave.jaccard@gmail.com>
2016-03-19 21:19:38 -0600

    progress

--- test/test_strategy.py
+++ test/test_strategy.py
@@ -1,16 +1,20 @@
 import unittest
-from t3.strategy import Strategy
+from t3.strategy import Strategy, TestStrategy
 
 class test_strategy(unittest.TestCase):
 
     s = None
 
     def setUp(self):
-        self.s = Strategy()
+        # Strategy is an abstract base class, it cannot be instantiated.
+        self.s = TestStrategy()
 
     def test_class(self):
         self.assertIsInstance(self.s, Strategy, "s is not Strategy")
 
+    def test_the_test(self):
+        expected = 1
+        self.assertEqual(expected, self.s.move())
 
 
 if __name__ == '__main__':
