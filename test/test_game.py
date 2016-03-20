b99a9178ebf9d90e319362e3be6b0b655cc1aa3b
ave19 <dave.jaccard@gmail.com>
2016-03-19 21:19:02 -0600

    not quite interaction proof yet

--- test/test_game.py
+++ test/test_game.py
@@ -1,7 +1,7 @@
 import unittest
 from t3.game import Game
 
-class test_game(unittest.TestCase):
+class test_game_init(unittest.TestCase):
 
     g = None
 
@@ -11,6 +11,19 @@ class test_game(unittest.TestCase):
     def test_class(self):
         self.assertIsInstance(self.g, Game, "s is not Game")
 
+
+# class test_game_interaction(unittest.TestCase):
+#
+#     g = None
+#
+#     def setUp(self):
+#         self.g = Game()
+#         self.g.board.board = self.g.board.convert_to_base3(8586)
+#         print "the board: " % self.g.board
+#
+#     def test_a_move(self):
+#         #self.g.play()
+#         pass
 
 
 if __name__ == '__main__':
